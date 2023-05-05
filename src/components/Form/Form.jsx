598f26cb6e73353a1a4d96932a21fcce7a7a6760
VirLg <19virlg@gmail.com>
2023-05-05 15:35:32 +0300

    add

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -15,21 +15,39 @@ state = {
     number: '',
     }
 
-handleNameChange=evt=>{
-    this.setState({name:evt.target.value})
-
+handleInputChange=evt=>{
+    this.setState({
+        [evt.target.name]:evt.target.value
+    })
+   
 }
 
 render(){
 return(
 <div>
     <form action="">
-        <label >
-            Ім'я<input type="text" name="Name" value={this.state.name}/>
+        <label>
+            Ім'я<input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={this.state.name}
+            onChange={this.handleInputChange}
+            />
         </label>
 
-        <label >
-            Номер<input type="text" name="NumberPhone" />
+        <label>
+            Номер<input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={this.state.number}
+            onChange={this.handleInputChange}
+            />  
         </label>
 
 <input type="submit"  />

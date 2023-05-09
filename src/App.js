eddfa8f741d258f107b96c9b9853a9b838d56848
VirLg <19virlg@gmail.com>
2023-05-10 02:34:23 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,29 +1,128 @@
-import PhoneBook from './components/PhoneBook/PhoneBook'
-import ContactForm from './components/ContactForm/ContactForm'
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-   
+import {Section} from './components/Section/Section'
+import {Contact} from './components/Contats/Contact'
 
-<div>
-  <PhoneBook />
-  <h1>Phonebook</h1>
-  <ContactForm  />
+class App extends React.Component{
 
-  {/* <h2>Contacts</h2>
-  <Filter ... />
-  <ContactList ... /> */}
-</div>
+    state = {
+        contacts: [
+          {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+          {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+          {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+          {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+        ],
+        filter: '',
+        name: '',
+        number: ''
+      }
+
+    handleInputChange=evt=>{
+        
+        const {target:{name,value}}=evt
+        this.setState({
+            
+            [name]:value,
+        })   
+    }
+
+    handleFormSubmitAddContact=evt=>{
+        evt.preventDefault()
+      this.setState({})
+        console.log(evt.target.element);
+    //    this.dataSubmit(this.state)
+        this.reset()
+    }
+
+
+ 
+
+    reset=()=>{
+        this.setState({
+       
+            contacts: [],
+            name: '',
+            number: '',
+            })
+    }
+
+    handleCheckBoxState(){
+
+        }  
+
+  
+        
 
 
 
 
 
 
+    render(){
+        return(
+
+         <>
+    <Section>
+         <form onSubmit={this.handleFormSubmitAddContact}>
+        <label>
+            Ім'я<input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={this.state.name}
+            onChange={this.handleInputChange}
+            />
+        </label>
+
+        <label>
+            Номер<input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={this.state.number}
+            onChange={this.handleInputChange}
+            />  
+        </label>
+
+<input type="submit"  />
+
+    </form>
+         </Section>
 
 
-  );
+    <Section>
+        
+
+        <Contact
+        contacts={this.state.contacts}
+
+
+        />
+      
+
+    </Section>
+
+         </>
+        )
+    }
 }
 
+
+
 export default App;
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/Contats/Contact.jsx
@@ -0,0 +1,22 @@
+
+
+export const Contact=function({contacts}){
+
+return(<ul>{contacts.map(({id,name,number})=>
+    
+    <li key={id}>
+        <span >{name}</span>
+        <span > {number}</span>      
+    </li>
+    )
+    }
+ 
+</ul>)
+} 
+
+
+
+
+
+
+
